f380c2d4237faffae8cd26891b6a44d064b1cbb1
ExoDraedon <officialstyles25@gmail.com>
2025-07-05 03:06:07 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Animación testeo 01
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
